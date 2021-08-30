d5fcb5ce6d2726fedf63b627fd337dc380107a53
naguilarm <gaguilar@acid.cl>
2021-08-29 22:04:29 -0400

    master

---
+++ FileWriter/FileWriter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                throw new Exception("Algo paso");
+            }
+            catch (Exception e)
+            {
+
+                Writer.WriteFile(e.Message);
+            }
+        }
+    }
+}
---
+++ FileWriter/FileWriter/Writer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWriter
+{
+    public static class Writer
+    {
+        public static void WriteFile(string msg)
+        {
+            try
+            {
+                var date = DateTime.Now;
+                string path = @"Logs\OutputExceptions-" + date.ToString("dd-MM-yyyy") + ".txt";
+                msg = date + " - " + msg;
+
+                string folder = @"\Logs";
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+                if (!File.Exists(path))
+                {
+                    // Create a file to write to.
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+                        sw.Write(msg + Environment.NewLine);
+                    }
+                }
+                else
+                {
+                    File.AppendAllText(path, msg + Environment.NewLine);
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
